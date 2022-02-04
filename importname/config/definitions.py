efe115c9ecfd0bfa3efda9e83e74d7678f5d6d5b
Henrik Hviid Hansen <HEHHA@orsted.dk>
2022-02-04 14:09:31 +0100

    added environment variable option to fetch data folder for data outside the project

--- importname/config/definitions.py
+++ importname/config/definitions.py
@@ -1,19 +1,21 @@
 import os
 from pathlib import Path
 
-# os.environ["DATASETS"]  # if data outside the project, point an environment variable "DATASETS" to the dir
-
 PATH_CONFIG = Path(__file__).parent
 PATH_SRC = PATH_CONFIG.parent  # optionally, change this to reflect the name of the 'importname' directory
 PATH_PROJECT_ROOT = PATH_SRC.parent
 PATH_DATA = PATH_PROJECT_ROOT / "data"
-PATH_DATA_RAW = PATH_DATA / "raw"
-PATH_DATA_INTERIM = PATH_DATA / "interim"
-PATH_DATA_PROCESSED = PATH_DATA / "processed"
+PATH_DATA_RAW = PATH_PROJECT_ROOT / "data" / "raw"
+PATH_DATA_INTERIM = PATH_PROJECT_ROOT / "data" / "interim"
+PATH_DATA_PROCESSED = PATH_PROJECT_ROOT / "data" / "processed"
 PATH_SAVED_MODELS = PATH_PROJECT_ROOT / "models"
 PATH_NOTEBOOKS = PATH_PROJECT_ROOT / "notebooks"
 PATH_REPORTS = PATH_PROJECT_ROOT / "reports"
 PATH_TESTS = PATH_PROJECT_ROOT / "tests"
+
+# if data outside the project, point an environment variable "DATASETS" to the dir and uncomment the line below
+# PATH_DATA = os.environ["DATASETS"]
+
 
 if __name__ == "__main__":
     try:
